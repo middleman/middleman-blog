a81a3025b7951aa8d35e96dda9514355eb43cc0b
Thomas Reynolds <me@tdreyno.com>
2012-06-03 12:20:58 -0700

    Doesn't need extension version any more

--- lib/middleman-blog.rb
+++ lib/middleman-blog.rb
@@ -4,7 +4,7 @@ require "middleman-blog/version"
 require "middleman-blog/template"
 require "middleman-blog/commands/article"
   
-::Middleman::Extensions.register(:blog, ">= 3.0.0.beta.3") do
+::Middleman::Extensions.register(:blog) do
   require "middleman-blog/extension"
   ::Middleman::Blog
 end
