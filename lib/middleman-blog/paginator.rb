3efdd55784ae09a89741a4901ef3710432b58d93
Tim Bates <tim@rumpuslabs.com>
2012-07-13 18:59:37 +0930

    Pagination tests and bug fixes

--- lib/middleman-blog/paginator.rb
+++ lib/middleman-blog/paginator.rb
@@ -16,11 +16,11 @@ module Middleman
           res.path
         else
           page_url = page_link.sub(":num", num.to_s)
-          index_re = %r{/#{Regexp.escape(@app.index_file)}$}
+          index_re = %r{(^|/)#{Regexp.escape(@app.index_file)}$}
           if res.path =~ index_re
-            res.path.sub(index_re, "/#{page_url}/#{@app.index_file}")
+            res.path.sub(index_re, "\\1#{page_url}/#{@app.index_file}")
           else
-            res.path.sub(%r{/([^/]*)\.([^/]*)$}, "/\\1/#{page_url}.\\2")
+            res.path.sub(%r{(^|/)([^/]*)\.([^/]*)$}, "\\1\\2/#{page_url}.\\3")
           end
         end
       end
@@ -59,11 +59,12 @@ module Middleman
               # The range of article numbers on this page
               # (1-based, for showing "Items X to Y of Z")
               'page_start' => 1,
-              'page_end' => per_page,
+              'page_end' => [per_page, articles.length].min,
 
-              # These contain the URL for the next and previous page.
+              # These contain the next and previous page.
               # They are set to nil if there are no more pages.
-              'next_page' => num_pages > 1 ? page_sub(res, 2, page_link) : nil,
+              # The nils are overwritten when the later pages are generated, below.
+              'next_page' => nil,
               'prev_page' => nil,
 
               # The list of articles for this page.
@@ -73,6 +74,8 @@ module Middleman
               # of "blog.articles" for consistency with the calendar and tag templates.
               'articles' => articles
             }
+
+            prev_page_res = res
 
             # Create additional resources for the 2nd and subsequent pages.
             (2..num_pages).each do |num|
@@ -85,6 +88,9 @@ module Middleman
               # Copy the metadata from the base page.
               p.add_metadata md
 
+              # Copy the proxy state from the base page.
+              p.proxy_to(res.proxied_to) if res.proxy?
+
               page_start = (num-1)*per_page
               page_end   = (num*per_page)-1
 
@@ -95,12 +101,19 @@ module Middleman
                 'num_pages' => num_pages,
                 'per_page' => per_page,
                 'page_start' => page_start+1,
-                'page_end' => page_end+1,
-                'next_page' => num < num_pages ? page_sub(res, num+1, page_link) : nil,
-                'prev_page' => page_sub(res, num-1, page_link),
+                'page_end' => [page_end+1, articles.length].min,
+                'next_page' => nil,
+                'prev_page' => prev_page_res,
                 'page_articles' => articles[page_start..page_end],
                 'articles' => articles
               }
+
+              # Add a reference in the previous page to this page
+              prev_page_res.add_metadata :locals => {
+                'next_page' => p
+              }
+
+              prev_page_res = p
 
               new_resources << p
             end
