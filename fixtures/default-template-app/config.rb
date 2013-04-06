66660cfde22557c6c026541dd726bea1854cd5ac
Thomas Reynolds <me@tdreyno.com>
2013-04-06 13:51:34 -0700

    make some blog data into param

--- fixtures/default-template-app/config.rb
+++ fixtures/default-template-app/config.rb
@@ -7,6 +7,9 @@ activate :directory_indexes
 
 activate :blog
 
+set :blog_name, "Deep Thoughts"
+set :blog_author, "Nick Adams"
+set :blog_avatar, "http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
 set :top_nav_title, { :title => "Home", :target => "index.html" }
 set :top_nav_items, [
   { :title => "About Me", :target => "about-me.html" },
@@ -15,6 +18,16 @@ set :top_nav_items, [
 ]
 
 helpers do
+  def page_title
+    title = blog_name.dup
+    if current_page.data.title
+      title << ": #{current_page.data.title}"
+    elsif is_blog_article?
+      title << ": #{current_article.title}"
+    end
+    title
+  end
+
   def link_to_with_active(title, url, class_name = 'active')
     active_class = (current_resource == sitemap.find_resource_by_path(url)) ? class_name : ''
     link_to(title, url, :class => active_class)
