35d73952477aa74bf40dd8ca7cac40762a364d2b
Ben Hollis <ben@benhollis.net>
2013-11-24 17:04:31 -0800

    Slightly clean template.rb

--- lib/middleman-blog/template.rb
+++ lib/middleman-blog/template.rb
@@ -6,27 +6,29 @@ module Middleman
     # A template that generates a blog-specific config.rb
     # and a set of example templates for index, layout, tags, and calendar.
     class Template < Middleman::Templates::Base
-      class_option "css_dir", 
-        :default => "stylesheets", 
+      class_option "css_dir",
+        :default => "stylesheets",
         :desc    => 'The path to the css files'
-      class_option "js_dir", 
-        :default => "javascripts", 
+      class_option "js_dir",
+        :default => "javascripts",
         :desc    => 'The path to the javascript files'
-      class_option "images_dir", 
-        :default => "images", 
+      class_option "images_dir",
+        :default => "images",
         :desc    => 'The path to the image files'
 
       def self.source_root
         File.join(File.dirname(__FILE__), 'template')
       end
-  
+
       def build_scaffold
         template "config.tt", File.join(location, "config.rb")
-        directory "source", File.join(location, "source")
-    
-        empty_directory File.join(location, "source", options[:css_dir])
-        empty_directory File.join(location, "source", options[:js_dir])
-        empty_directory File.join(location, "source", options[:images_dir])
+
+        source = File.join(location, "source")
+        directory "source", source
+
+        [:css_dir, :js_dir, :images_dir].each do |dir|
+          empty_directory File.join(source, options[dir])
+        end
       end
     end
   end
