45bfb9c9b077e54334033d11b97a34afa9f053ad
Thomas Reynolds <me@tdreyno.com>
2016-03-22 12:55:07 -0700

    Add back article command

--- lib/middleman-blog.rb
+++ lib/middleman-blog.rb
@@ -1,8 +1,8 @@
 require "middleman-core"
 require "middleman-blog/version"
-require "middleman-blog/commands/article"
 
 ::Middleman::Extensions.register(:blog) do
   require "middleman-blog/extension"
+  require "middleman-blog/commands/article"
   ::Middleman::BlogExtension
 end
--- lib/middleman-blog/commands/article.rb
+++ lib/middleman-blog/commands/article.rb
@@ -5,54 +5,74 @@ require 'middleman-blog/uri_templates'
 module Middleman
   module Cli
     # This class provides an "article" command for the middleman CLI.
-    class Article < Thor
+    class Article < ::Thor::Group
       include Thor::Actions
       include Blog::UriTemplates
 
       check_unknown_options!
 
-      namespace :article
-
+      # Template files are relative to this file
+      # @return [String]
       def self.source_root
-        ENV['MM_ROOT']
+        File.dirname(__FILE__)
       end
 
-      # Tell Thor to exit with a nonzero exit code on failure
-      def self.exit_on_failure?
-        true
-      end
+      argument :title, type: :string
 
-      desc "article TITLE", "Create a new blog article"
-      method_option "date",
+      class_option "date",
         aliases: "-d",
         desc: "The date to create the post with (defaults to now)"
-      method_option "lang",
+      class_option "lang",
+        desc: "Deprecated, use locale"
+      class_option "locale",
         aliases: "-l",
-        desc: "The language to create the post with (defaults to I18n.default_locale if avaliable)"
-      method_option "blog",
+        desc: "The locale to create the post with (defaults to I18n.default_locale if avaliable)"
+      class_option "blog",
         aliases: "-b",
         desc: "The name of the blog to create the post inside (for multi-blog apps, defaults to the only blog in single-blog apps)"
-      def article(title)
-        shared_instance = ::Middleman::Application.server.inst
+      def article
+        config = ::Middleman::Cli.config
 
-        # This only exists when the config.rb sets it!
-        if shared_instance.respond_to? :blog
-          @title = title
-          @slug = safe_parameterize(title)
-          @date = options[:date] ? Time.zone.parse(options[:date]) : Time.zone.now
-          @lang = options[:lang] || ( I18n.default_locale if defined? I18n )
+        @title = title
+        @slug = safe_parameterize(title)
+        @date = options[:date] ? ::Time.zone.parse(options[:date]) : Time.zone.now
+        @lang = options[:lang] || options[:locale] || (::I18n.default_locale if defined? ::I18n )
 
-          blog_inst = shared_instance.blog(options[:blog])
-
-          path_template = blog_inst.source_template
-          params = date_to_params(@date).merge(lang: @lang.to_s, title: @slug)
-          article_path = apply_uri_template path_template, params
-
-          template blog_inst.options.new_article_template, File.join(shared_instance.source_dir, article_path + blog_inst.options.default_extension)
-        else
-          raise Thor::Error.new "You need to activate the blog extension in config.rb before you can create an article"
+        app = ::Middleman::Application.new do
+          config[:mode] = :config
+          config[:disable_sitemap] = true
+          config[:watcher_disable] = true
+          config[:exit_before_ready] = true
         end
+
+        blog_inst = if options[:blog]
+          app.extensions[:blog][options[:blog]]
+        else
+          app.extensions[:blog].values.first
+        end
+
+        unless blog_inst
+          msg = "Could not find an active blog instance"
+          msg << " named #{options[:blog]}" if options[:blog]
+          throw msg
+        end
+
+        path_template = blog_inst.data.source_template
+        params = date_to_params(@date).merge(lang: @lang.to_s, locale: @locale.to_s, title: @slug)
+        article_path = apply_uri_template path_template, params
+
+        template blog_inst.options.new_article_template, File.join(app.source_dir, article_path + blog_inst.options.default_extension)
       end
+
+      protected
+
+      def blog_instance(key)
+        return nil unless app.extensions[:blog]
+        return app.extensions[:blog][key]
+      end
+
+      # Add to CLI
+      Base.register(self, 'article', 'article TITLE [options]', 'Create a new blog article')
     end
   end
 end
