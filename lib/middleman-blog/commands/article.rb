5210252cfd0f590e03498640108026b8ace5e120
Ian Warner <ian.warner@drykiss.com>
2017-05-17 17:34:20 +0200

    Added tags and content to the article command line

--- lib/middleman-blog/commands/article.rb
+++ lib/middleman-blog/commands/article.rb
@@ -1,127 +1,136 @@
-﻿require 'middleman-core/cli'
+require 'middleman-core/cli'
 require 'date'
 require 'middleman-blog/uri_templates'
 
 module Middleman
 
-    module Cli
+  module Cli
 
-        ##
-        # This class provides an "article" command for the middleman CLI.
-        #
-        # @usage bundle exec middleman article --help
-        # @usage bundle exec middleman article "Article Title"
-        # @usage bundle exec middleman article "Article Title" --date 2017-03-25 --lang fr-be --blog blog_fr-be --subdirectory --edit
-        #
-        # @see   https://middlemanapp.com/basics/blogging/#generating-articles
-        ##
-        class Article < ::Thor::Group
+    ##
+    # This class provides an "article" command for the middleman CLI.
+    #
+    # @usage bundle exec middleman article --help
+    # @usage bundle exec middleman article "Article Title"
+    # @usage bundle exec middleman article "Article Title" -t "test,test1" -c "<b>String</b>" -d 2017-03-25 -l fr-be -b blog_fr-be -s -e
+    #
+    # @see   https://middlemanapp.com/basics/blogging/#generating-articles
+    #
+    # @todo Tags should be removed from the template if they are not required
+    ##
+    class Article < ::Thor::Group
 
-            include Thor::Actions
-            include Blog::UriTemplates
+      include Thor::Actions
+      include Blog::UriTemplates
 
-            check_unknown_options!
+      check_unknown_options!
 
-            # Template files are relative to this file
-            # @return [String]
-            def self.source_root
-                File.dirname( __FILE__ )
-            end
+      # Template files are relative to this file
+      # @return [String]
+      def self.source_root
+        File.dirname( __FILE__ )
+      end
 
-            argument :title, type: :string
+      argument :title, type: :string
 
-            class_option "date",
-                aliases: "-d",
-                desc:    "The date to create the post with (defaults to now)"
+      class_option "blog",
+        aliases: "-b",
+        desc:    "The name of the blog to create the post inside (for multi-blog apps, defaults to the only blog in single-blog apps)"
 
-            class_option "lang",
-                desc: "Deprecated, use locale"
+      class_option "content",
+        aliases: "-c",
+        desc: "Content of the post"
 
-            class_option "locale",
-                aliases: "-l",
-                desc:    "The locale to create the post with (defaults to I18n.default_locale if available)"
+      class_option "date",
+        aliases: "-d",
+        desc:    "The date to create the post with (defaults to now)"
 
-            class_option "blog",
-                aliases: "-b",
-                desc:    "The name of the blog to create the post inside (for multi-blog apps, defaults to the only blog in single-blog apps)"
+      class_option "locale",
+        aliases: "-l",
+        desc:    "The locale to create the post with (defaults to I18n.default_locale if available)"
 
-            class_option "edit",
-                aliases: "-e",
-                desc:    "Edit the newly created blog post",
-                default: false,
-                type:    :boolean
+      class_option "edit",
+        aliases: "-e",
+        desc:    "Edit the newly created blog post",
+        default: false,
+        type:    :boolean
 
-            class_option "subdirectory",
-                aliases: "-s",
-                desc:    "Generate an article subdirectory (for directory indexes, defaults to false)",
-                default: false,
-                type:    :boolean
+      class_option "subdirectory",
+        aliases: "-s",
+        desc:    "Generate an article subdirectory (for directory indexes, defaults to false)",
+        default: false,
+        type:    :boolean
 
-            def article
+      class_option "tags",
+        aliases: "-t",
+        desc: "A list of comma-separated tags for the post"
 
-                @title = title
-                @slug  = safe_parameterize( title )
-                @date  = options[ :date ] ? ::Time.zone.parse( options[ :date ] ) : Time.zone.now
-                @lang  = options[ :lang ] || options[ :locale ] || ( ::I18n.default_locale if defined? ::I18n )
+      def article
 
-                app = ::Middleman::Application.new do
-                    config[ :mode ]              = :config
-                    config[ :disable_sitemap ]   = true
-                    config[ :watcher_disable ]   = true
-                    config[ :exit_before_ready ] = true
-                end
+        @content = options[:content] || ""
+        @date    = options[ :date ] ? ::Time.zone.parse( options[ :date ] ) : Time.zone.now
+        @locale  = options[ :locale ] || ( ::I18n.default_locale if defined? ::I18n )
+        @slug    = safe_parameterize( title )
+        @tags    = options[:tags] && options[:tags].split(/\s*,\s*/) || []
+        @title   = title
 
-                blog_inst = if options[ :blog ]
-                    app.extensions[ :blog ].find { | key, instance | instance.options[ :name ] == options[ :blog ] }[ 1 ]
-                else
-                    app.extensions[ :blog ].values.first
-                end
+        app = ::Middleman::Application.new do
+          config[ :mode ]              = :config
+          config[ :disable_sitemap ]   = true
+          config[ :watcher_disable ]   = true
+          config[ :exit_before_ready ] = true
+        end
 
-                unless blog_inst
-                    msg = "Could not find an active blog instance"
-                    msg << " named #{ options[ :blog ] }" if options[ :blog ]
-                    throw msg
-                end
+        blog_inst = if options[ :blog ]
+          app.extensions[ :blog ].find { | key, instance | instance.options[ :name ] == options[ :blog ] }[ 1 ]
+        else
+          app.extensions[ :blog ].values.first
+        end
 
-                path_template         = blog_inst.data.source_template
-                params                = date_to_params( @date ).merge( lang: @lang.to_s, locale: @locale.to_s, title: @slug )
-                article_path          = apply_uri_template path_template, params
-                absolute_article_path = File.join( app.source_dir, article_path + blog_inst.options.default_extension )
+        unless blog_inst
+          msg = "Could not find an active blog instance"
+          msg << " named #{ options[ :blog ] }" if options[ :blog ]
+          throw msg
+        end
 
-                template blog_inst.options.new_article_template, absolute_article_path
+        path_template         = blog_inst.data.source_template
+        params                = date_to_params( @date ).merge( locale: @locale.to_s, title: @slug )
+        article_path          = apply_uri_template path_template, params
+        absolute_article_path = File.join( app.source_dir, article_path + blog_inst.options.default_extension )
 
-                # Edit option process
-                if options[ :edit ]
+        template blog_inst.options.new_article_template, absolute_article_path
 
-                    editor = ENV.fetch( 'MM_EDITOR', ENV.fetch( 'EDITOR', nil ) )
+        # Edit option process
+        if options[ :edit ]
 
-                    if editor
-                        system( "#{ editor } #{ absolute_article_path }" )
-                    else
-                        throw "Could not find a suitable editor. Try setting the environment variable MM_EDITOR."
-                    end
+          editor = ENV.fetch( 'MM_EDITOR', ENV.fetch( 'EDITOR', nil ) )
 
-                end
-
-                # Subdirectory option process
-                if options[ :subdirectory ]
-                    empty_directory extract_directory_path( File.join( app.source_dir, article_path ) )
-                end
-
-            end
-
-            protected
-
-            def blog_instance( key )
-                return nil unless app.extensions[ :blog ]
-                return app.extensions[ :blog ][ key ]
-            end
-
-            # Add to CLI
-            Base.register( self, 'article', 'article TITLE [options]', 'Create a new blog article' )
+          if editor
+            system( "#{ editor } #{ absolute_article_path }" )
+          else
+            throw "Could not find a suitable editor. Try setting the environment variable MM_EDITOR."
+          end
 
         end
 
+        # Subdirectory option process
+        if options[ :subdirectory ]
+          empty_directory extract_directory_path( File.join( app.source_dir, article_path ) )
+        end
+
+      end
+
+      protected
+
+      def blog_instance( key )
+        return nil unless app.extensions[ :blog ]
+        return app.extensions[ :blog ][ key ]
+      end
+
+      # Add to CLI
+      Base.register( self, 'article', 'article TITLE [options]', 'Create a new blog article' )
+
     end
+
+  end
 
 end
